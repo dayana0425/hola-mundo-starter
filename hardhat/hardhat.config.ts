fe814c1e10a16228fc46a32afeb571206818ea9c
Daiana Bilbao <dbilbao@cs.colostate.edu>
2022-07-30 18:52:41 -0600

    got rid of monorepo

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,5 +1,4 @@
-import * as dotenv from "dotenv";
-
+import "dotenv/config";
 import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
@@ -8,20 +7,23 @@ import "hardhat-gas-reporter";
 import "solidity-coverage";
 import "hardhat-abi-exporter";
 
-dotenv.config();
-
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+task("accounts", "Prints the list of accounts", async (_taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
-
   for (const account of accounts) {
     console.log(account.address);
   }
 });
 
+task("hello", "Prints 'Hello, World!'", async () => {
+  console.log("Hello, World!");
+});
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
+
+const { MUMBAI_API_URL, GOERLI_API_URL, PRIVATE_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
@@ -31,13 +33,19 @@ const config: HardhatUserConfig = {
       chainId: 1337,
     },
     goerli: {
-      url: `${process.env.GOERLI_ALCHEMY_KEY}`,
-      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      url: GOERLI_API_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
     },
     mumbai: {
-      url: `${process.env.MUMBAI_ALCHEMY_KEY}`,
-      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      url: MUMBAI_API_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
     },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
   },
   abiExporter: {
     path: "../next-app/utils/ABI",
